32ef7119bbe0130154fe8c4b123381e9833089db
Diva Krishna <divakrishnam@gmail.com>
2021-11-22 21:34:50 +0800

    heroku setup

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 
+app.get('/', (req, res) => {
+  res.send('jobs api');
+});
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
